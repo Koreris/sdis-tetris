dd761d7750acbed84f60aaf4ff9906f46f35a140
JoseRVG <zedeco2010@hotmail.com>
2018-04-29 18:41:20 +0100

    Add MultiPlayer Interface

---
+++ core/src/com/sdis/tetris/gui/GUICreateLoby.java
@@ -0,0 +1,143 @@
+package com.sdis.tetris.gui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.loaders.AssetLoader;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.sdis.tetris.Buttons;
+import com.sdis.tetris.Tetris;
+import com.sdis.tetris.audio.SFX;
+import com.sdis.tetris.audio.Song;
+
+public class GUICreateLoby extends GUIScreen{
+
+    private final Stage stage = new Stage();
+    private final Table table = new Table();
+    Sprite background = new Sprite(new Texture(Gdx.files.internal("img/main_menu.png"), false));
+    Sprite title = new Sprite(new Texture(Gdx.files.internal("img/main_title.png"), false));
+    private final TextButton createButton = new TextButton("Create_Loby", Buttons.MenuButton);
+    private final TextButton backButton = new TextButton("MultiPlayer_Menu", Buttons.MenuButton);
+    private Label label;
+    private Skin skin;
+    private TextField usernameTextField;
+
+    private class Back implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            parent.switchTo(new GUIMultiPlayer(parent));
+        }
+    }
+
+    private class CreateLoby implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            parent.switchTo(new GUIWaitLoby(parent));
+        }
+    }
+
+    public GUICreateLoby(Tetris paramParent) {
+        super(paramParent, Song.THEME_A);
+        background.setPosition(0,0);
+        background.setSize((float)Gdx.graphics.getWidth(),(float)Gdx.graphics.getHeight());
+        skin  = new Skin(Gdx.files.internal("menu/menu.json"), new TextureAtlas(Gdx.files.internal("menu/menu.atlas")));
+        label = new Label("Loby name:", skin);
+        usernameTextField = new TextField("", skin);
+        usernameTextField.setAlignment(Align.center);
+        table.add(label);
+        table.add(usernameTextField).width(200);
+        table.row();
+        title.setPosition((float)Gdx.graphics.getWidth()/3.7f,(float)Gdx.graphics.getHeight()-title.getHeight()*2);
+        table.add(createButton).size((float)Gdx.graphics.getWidth()/2, (float)Gdx.graphics.getHeight()/8).padBottom(10).row();
+        table.add(backButton).size((float)Gdx.graphics.getWidth()/2, (float)Gdx.graphics.getHeight()/8).padBottom(10).row();
+        table.setFillParent(true);
+        table.setVisible(true);
+        stage.addActor(table);
+        backButton.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                audio.playSFX(SFX.HOVER);
+                stage.addAction(Actions.sequence(Actions.moveTo(-480.0f, 0.0f, 0.5f), Actions.run(new Back())));
+            }
+
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor)
+            {
+                if (! backButton.isPressed())
+                {
+                    audio.playSFX(SFX.SELECT);
+                }
+            }
+        });
+        createButton.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                audio.playSFX(SFX.HOVER);
+                stage.addAction(Actions.sequence(Actions.moveTo(-480.0f, 0.0f, 0.5f), Actions.run(new CreateLoby())));
+            }
+
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor)
+            {
+                if (! createButton.isPressed())
+                {
+                    audio.playSFX(SFX.SELECT);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor( 1, 1, 1, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        stage.act();
+
+        batch.begin();
+
+        background.draw(batch);
+        title.draw(batch);
+
+        batch.end();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+        stage.getViewport().update(width, height);
+        //camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
+    }
+
+    @Override
+    public void show()
+    {
+        Gdx.input.setInputProcessor(stage);
+
+    }
+
+    @Override
+    public void dispose()
+    {
+        stage.dispose();
+        skin.dispose();
+    }
+}
